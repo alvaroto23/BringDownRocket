b6c2d446ae2dfaf2a8d30410322b52d601a81241
alvaroto23 <alvaro231099@gmail.com>
2024-12-09 15:28:57 +0100

    Añadir audio y limites del XWing
    Además del audio, he quitado campos innecesarios en los limites del XWing

--- Assets/Scripts/XWing.cs
+++ Assets/Scripts/XWing.cs
@@ -10,9 +10,6 @@ public class XWing : MonoBehaviour
     [SerializeField] private Transform ship;
     private Vector2 moveInput;
 
-    private float newX;
-    private float newY;
-
     private float limitX = 8;
     private float limitY = 4;
 
@@ -26,38 +23,30 @@ public class XWing : MonoBehaviour
             // Esto conserva la orientación tras soltar la tecla hacia donde estaba la ultima vez su UP, osea, el eje y en positivo, que se ha desorientado en su posición local
             //ship.up = moveInput;
         }
-
         checkCollision();
-
     }
-
-    
+        
     private void OnMove(InputValue input)
     {
         moveInput = input.Get<Vector2>();
     }
 
-
-
     private void checkCollision()
     {
-        newX = transform.position.x;
-        newY = transform.position.y;
-
-        if (newX <= -limitX)
+        if (transform.position.x <= -limitX)
         {
             transform.position = new Vector3 (-limitX, transform.position.y, transform.position.z);
         }
-        else if (newX >= limitX)
+        else if (transform.position.x >= limitX)
         {
             transform.position = new Vector3(limitX, transform.position.y, transform.position.z);
         }
 
-        if (newY <= -limitY)
+        if (transform.position.y <= -limitY)
         {
             transform.position = new Vector3(transform.position.x, -limitY, transform.position.z);
         }
-        else if (newY >= limitY)
+        else if (transform.position.y >= limitY)
         {
             transform.position = new Vector3(transform.position.x, limitY, transform.position.z);
         }
---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class gameController : MonoBehaviour
+{
+
+    [SerializeField] private AudioMixer mixer;
+    private bool pauseGame = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseGame =! pauseGame;
+        }
+        
+        if (pauseGame)
+        {
+            mixer.SetFloat("LowpassFreq", 500);
+            Time.timeScale = 0f;
+        }
+
+        else if (!pauseGame)
+        {
+            mixer.SetFloat("LowpassFreq", 22000);
+            Time.timeScale = 1f;
+        }
+
+
+    }
+}
