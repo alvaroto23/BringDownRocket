34ca352a016f872f508b54e64e3cd6ff6ecbbcf1
alvaroto23 <alvaro231099@gmail.com>
2024-12-09 15:18:04 +0100

    Pasar el audio de disparo de los shoots al shootspawner

--- Assets/Scripts/ShootSpawner.cs
+++ Assets/Scripts/ShootSpawner.cs
@@ -8,9 +8,18 @@ public class ShootSpawner : MonoBehaviour
     [SerializeField] private GameObject shoot;
     [SerializeField] private Transform parentPos;
 
+    private AudioSource audioController;
+    [SerializeField] private AudioClip shootClip;
+
+
+    private void Start()
+    {
+        audioController = GetComponent<AudioSource>();
+    }
     private void OnShoot ()
     {
         Vector3 cannon = new Vector3(parentPos.position.x, parentPos.position.y, 0);
+        audioController.PlayOneShot(shootClip);
 
         // (Prefab a instnacias, posición inicial, rotación inicial)
         // Quaternion.identity viene a ser rotación 0
