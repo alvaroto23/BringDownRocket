351e239ab5954e69c1e26b24b0e0886ffa402834
alvaroto23 <alvaro231099@gmail.com>
2024-12-09 16:47:01 +0100

    Que la música vaya con input system y no poder disparar en pause

--- Assets/Scripts/ShootSpawner.cs
+++ Assets/Scripts/ShootSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +8,7 @@ public class ShootSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject shoot;
     [SerializeField] private Transform parentPos;
+    private bool canShoot = true;
 
     private AudioSource audioController;
     [SerializeField] private AudioClip shootClip;
@@ -16,13 +18,22 @@ public class ShootSpawner : MonoBehaviour
     {
         audioController = GetComponent<AudioSource>();
     }
+
+    public void shootPosible()
+    {
+        canShoot = !canShoot;
+    }
+
     private void OnShoot ()
     {
+        if (canShoot)
+        {
         Vector3 cannon = new Vector3(parentPos.position.x, parentPos.position.y, 0);
         audioController.PlayOneShot(shootClip);
 
         // (Prefab a instnacias, posición inicial, rotación inicial)
         // Quaternion.identity viene a ser rotación 0
         Instantiate(shoot, cannon, Quaternion.identity);
+        }
     }
 }
--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -1,31 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Audio;
 
 public class gameController : MonoBehaviour
 {
 
+    [SerializeField] private ShootSpawner shooter;
     [SerializeField] private AudioMixer mixer;
     private bool pauseGame = false;
 
-    // Update is called once per frame
-    void Update()
+    private void OnPauseAndPlay ()
     {
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            pauseGame =! pauseGame;
-        }
+        pauseGame =! pauseGame;
         
         if (pauseGame)
         {
+            shooter.shootPosible();
             mixer.SetFloat("LowpassFreq", 500);
             Time.timeScale = 0f;
         }
 
         else if (!pauseGame)
         {
+            shooter.shootPosible();
             mixer.SetFloat("LowpassFreq", 22000);
             Time.timeScale = 1f;
         }
